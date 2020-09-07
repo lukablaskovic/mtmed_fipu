d98c193cf005771a14ccc1b0be5d2bf12fe629d0
Luka Blaskovic <lukablaskovic2000@gmail.com>
2020-09-07 18:24:27 +0200

    script change

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ var modal = document.getElementById("myModal");
 var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+window.onload = function(){
+  img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+};
+
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
